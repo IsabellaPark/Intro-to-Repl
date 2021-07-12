cc6731759ebbfc92a906983a50d191e64b320a6e
ParkBella <87280152+IsabellaPark@users.noreply.github.com>
2021-07-12 22:34:47 +0000

    some rules, and variables

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 print("Hello World")
+#Question "how old are you?" and the answer
 age = input("how old are you?")
 print(age)
+
+#Data types:
+#string characters/words
+#float/double decimal numbers
+#integer whole numbers
+#boolean true/false
+
+var1 = "hello world"
+var2 = 42.5
+var3 = 20
+var4 = True
+
+print(var1)
+print(var2)
+print(var3)
+print(var4)
